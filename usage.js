82000ff87a1348392327a8f3a4b34b64c9a706d5
OmriSh <OmriShilian@gmail.com>
2017-03-13 21:27:24 +0200

    base usage

---
+++ usage.js
@@ -0,0 +1,34 @@
+const ClassName = require('module-name');
+
+var myExp = "x.test > 55";
+var myObject = { x: 5 };
+
+var func = ClassName.compile(myExp);
+var result = func.test(myObject);
+
+//--------------------------------------------------
+
+//on base proto
+biggenThan={
+    type: 'operator',
+    token: '>', //on debug only
+    ref: function(x, y){ return x > b; }
+}
+
+someConst={
+    type: 'const',
+    token: '5', //on debug only
+    value: 5
+}
+
+accessor={
+    type: 'access',
+    token: 'x.test', //on debug only
+    access: function(){ return x['test']; }
+}
+
+root = Object.create(biggenThan);
+root.left = someConst;
+root.right = accessor;
+
+//root is the compiled predicate
